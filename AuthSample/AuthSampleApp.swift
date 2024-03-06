cda2ac03187dfc660207ec7cf43018bd7fb858d8
Martin Poulsen <martin.poulsen@elitappar.com>
2024-03-06 08:36:25 +0100

    Initial Commit

---
+++ AuthSample/AuthSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSampleApp.swift
+//  AuthSample
+//
+//  Created by Martin Poulsen on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct AuthSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthSample
+//
+//  Created by Martin Poulsen on 2024-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
